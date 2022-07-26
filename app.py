3922ebaf014d4604d231d3ab74ba09a7e56eed14
VictorHOG <victor.hugo.ortega@correounivalle.edu.co>
2022-07-26 01:09:12 -0500

    Cargar archivos para el modelo 1

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
 from flask import Flask, render_template, request
 from processing import processFile, executeMinizinc
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "../Modelo"
+ALLOWED_EXTENSIONS = set(['dzn'])
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
+        # form_name = request.form['form-name']
+        # print(form_name)
+        form = request.form
         processFile(request)
         result = executeMinizinc()
-        return render_template('index.html', result=result)
+        return render_template('index.html', result=result, form=form)
     elif request.method == 'GET':   
         return render_template('index.html')
+
+def allowed_file(file):
+    file = file.split('.')
+    print(file)
+    if file[1] in ALLOWED_EXTENSIONS:
+        return True
+    return False
+
+@app.route('/uploadFile', methods=['POST'])
+def uploadFile():
+
+    file = request.files["uploadFile"]
+    print(file, file.filename)
+    filename = secure_filename(file.filename)
+    print(filename)
+    if file and allowed_file(filename):
+        print("permitido")
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        os.remove("../Modelo/Datos.dzn")
+        os.rename("../Modelo/" + filename, "../Modelo/Datos.dzn")
+
+    result = executeMinizinc()
+    return render_template('index.html', result=result)
+    
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
